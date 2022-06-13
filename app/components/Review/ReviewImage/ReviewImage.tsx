368418bde8c285d80664ec4057b6642b78b1ed5b
Peter Eck <jpeckiii@gmail.com>
2022-06-13 17:07:38 -0500

    Optimize review image component for tablet and desktop

--- app/components/Review/ReviewImage/ReviewImage.tsx
+++ app/components/Review/ReviewImage/ReviewImage.tsx
@@ -8,14 +8,13 @@ export default function ReviewImage({
   bottleName,
 }: ReviewImageProps) {
   return (
-    <div>
-      <figure>
-        <picture>
-          <source media="(min-width: 640px)" srcSet={imageUrl} />
-          <img src={imageUrl} alt={`Bottle of ${bottleName}`} />
-          {/* Consider adding object-fit: cover to image to maintain aspect ratio */}
-        </picture>
-      </figure>
+    <div className="flex justify-center">
+      <img
+        src={imageUrl}
+        alt={`Bottle of ${bottleName}`}
+        className="object-contain"
+      />
+      {/* Consider adding object-fit: cover to image to maintain aspect ratio */}
     </div>
   );
 }
